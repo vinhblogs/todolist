763a94e1149a0174e1ec8b4b58533c0ae040b66a
Quang Vinh <qvinh10420@gmail.com>
2023-03-15 14:19:17 +0700

    //todolist_icon

--- todo_git_test/src/App.js
+++ todo_git_test/src/App.js
@@ -1,10 +1,48 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import { FaTrashAlt } from 'react-icons/fa';
 
 function App() {
+
+  
+  const [jobs,setJobs]=useState([])
+  const [job,setJob]=useState('')
+
+
+  // handle submit
+  const handleSubmit = ()=>{
+    setJobs(prev =>{
+      const newJobs=[...prev,job]
+
+      // save local
+      localStorage.setItem("jobs",JSON.stringify(newJobs))
+      return newJobs;
+    })
+    setJob('')
+  }
+
+
+  // handle delete
+  const handleDelete= (index)=>{
+    const removeItem=jobs.filter((job,key)=>{
+      return key!==index
+    })
+    setJobs(removeItem)
+  }
   return (
+    
     <div className="App">
-      <p>aaaaaa</p>
+      <h1>todo-list</h1>
+
+      <input value={job} onChange={(e)=> setJob(e.target.value)} placeholder="input"></input>
+      <button onClick={()=>handleSubmit()}>Add</button>
+      <ul>
+        {jobs.map((job,index)=>(
+          <li key={index}>{index +1}-{job}  <button onClick={()=>handleDelete(index)}><FaTrashAlt /></button></li>
+          
+        ))}
+      </ul>
     </div>
   );
 }
